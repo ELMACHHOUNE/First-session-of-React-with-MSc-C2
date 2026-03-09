4b1c72c55a89946a59f004904fc1205551f33c4f
ELMACHHOUNE <mohamed20rida@gmail.com>
2026-03-09 20:52:29 +0000

    first project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className="bg-gray-800 p-4 mt-8">
+        <div className="container mx-auto text-center text-white">
+          <p>&copy; 2026 My Portfolio. All rights reserved.</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
